71ba9c23252a0d6eb7a6ebd30fda668edb88a292
jonathanprozzi <jonathanprozzi@gmail.com>
2021-07-09 00:08:54 -0400

    styling docs pages

---
+++ src/components/SocialLinksSection.js
@@ -0,0 +1,16 @@
+import * as React from "react";
+import styles from "./SocialLinksSection.module.css";
+
+const SocialLinksSection = () => {
+  return (
+    <section className={styles.socialLinksContainer}>
+      <div>
+        <div>
+          <a href=''></a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default SocialLinksSection;
