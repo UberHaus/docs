b70d062687399bf1c6c854900dfa81dfa9f5efea
jonathanprozzi <jonathanprozzi@gmail.com>
2021-07-16 17:44:46 -0400

    added SocialIcons component, SocialLinksSection

--- src/components/SocialLinksSection.js
+++ src/components/SocialLinksSection.js
@@ -1,6 +1,14 @@
 import * as React from "react";
 import styles from "./SocialLinksSection.module.css";
 
+const SocialLinks = [
+  {
+    name: "Discord",
+    link: "discord.gg",
+    icon: "",
+  },
+];
+
 const SocialLinksSection = () => {
   return (
     <section className={styles.socialLinksContainer}>
