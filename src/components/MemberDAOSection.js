a6f34fcffe63d1afd7988a0124fdca4cbb049f80
jonathanprozzi <jonathanprozzi@gmail.com>
2021-07-16 18:51:59 -0400

    added second Member section with linear gradient background (this is why its a new section)

--- src/components/MemberDAOSection.js
+++ src/components/MemberDAOSection.js
@@ -3,33 +3,66 @@ import styles from "./MemberDAOSection.module.css";
 
 const MemberDAOSection = () => {
   return (
-    <section className={styles.memberContainer}>
-      <div className={styles.contentGrid}>
-        <div className={styles.memberIllustrationContainer}>
-          <img
-            src='/img/Illustration_BecomeMember.jpeg'
-            alt=''
-            className={styles.memberllustration}
-          />
+    <section>
+      <div className={styles.memberFirstContainer}>
+        <div className={styles.firstContentGrid}>
+          <div className={styles.memberIllustrationContainer}>
+            <img
+              src='/img/Illustration_BecomeMember.jpeg'
+              alt=''
+              className={styles.memberllustration}
+            />
+          </div>
+          <div className={styles.flexMemberCTA}>
+            <h2 className={styles.memberCTAHeading}>Become a member DAO</h2>
+            <p className={styles.memberCTACopy}>
+              If you want to join UberHaus, there are several things you should
+              do before submitting an official proposal to the DAO. Visit the
+              UberHaus Forum and select “+ New Topic”.
+              <br />
+              <br />
+              This is where you will want to showcase what your DAO can do for
+              UberHaus.
+            </p>
+            <div className={styles.buttonsContainer}>
+              <a href='#' className={styles.memberCTAButton}>
+                Go to Forum ➔
+              </a>
+              <a href='#' className={styles.memberSecondaryButton}>
+                Learn More ➔
+              </a>
+            </div>
+          </div>
         </div>
-        <div className={styles.flexMemberCTA}>
-          <h2 className={styles.memberCTAHeading}>Become a member DAO</h2>
-          <p className={styles.memberCTACopy}>
-            If you want to join UberHaus, there are several things you should do
-            before submitting an official proposal to the DAO. Visit the
-            UberHaus Forum and select “+ New Topic”.
-            <br />
-            <br />
-            This is where you will want to showcase what your DAO can do for
-            UberHaus.
-          </p>
-          <div className={styles.buttonsContainer}>
-            <a href='#' className={styles.memberCTAButton}>
-              Go to Forum ➔
-            </a>
-            <a href='#' className={styles.memberSecondaryButton}>
-              Learn More ➔
-            </a>
+        <div className={styles.memberSecondContainer}>
+          <div className={styles.secondContentGrid}>
+            <div className={styles.flexMemberCTA}>
+              <h2 className={styles.memberCTAHeading}>Become a member DAO</h2>
+              <p className={styles.memberCTACopy}>
+                If you want to join UberHaus, there are several things you
+                should do before submitting an official proposal to the DAO.
+                Visit the UberHaus Forum and select “+ New Topic”.
+                <br />
+                <br />
+                This is where you will want to showcase what your DAO can do for
+                UberHaus.
+              </p>
+              <div className={styles.buttonsContainer}>
+                <a href='#' className={styles.memberCTAButton}>
+                  Go to Forum ➔
+                </a>
+                <a href='#' className={styles.memberSecondaryButton}>
+                  Learn More ➔
+                </a>
+              </div>
+            </div>
+            <div className={styles.memberIllustrationContainer}>
+              <img
+                src='/img/Illustration_BecomeMember.jpeg'
+                alt=''
+                className={styles.memberllustration}
+              />
+            </div>
           </div>
         </div>
       </div>
