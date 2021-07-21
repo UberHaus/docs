6d1c8454fdf0393c92c5f2fe6637ecc7583809ef
jonathanprozzi <jonathanprozzi@gmail.com>
2021-07-20 22:13:18 -0400

    added each section back in now that each has been checked for responsive layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,29 +16,12 @@ export default function Home() {
   const { siteConfig = {} } = context;
   return (
     <Layout title={`${siteConfig.title}`} description='A HAUS for your DAO.'>
-      {/* <header className={clsx("hero hero--primary", styles.heroBanner)}>
-        <div className='container'>
-          <h1 className='hero__title'>{siteConfig.title}</h1>
-          <p className='hero__subtitle'>{siteConfig.tagline}</p>
-          <div className={styles.buttons}>
-            <Link
-              className={clsx(
-                "button button--outline button--secondary button--lg",
-                styles.getStarted
-              )}
-              to={useBaseUrl("docs/welcome")}
-            >
-              Get Started
-            </Link>
-          </div>
-        </div>
-      </header> */}
       <main>
         <HeroSection />
-        {/* <IntroSection /> */}
-        {/* <CommunitySection /> */}
-        {/* <SocialLinksSection /> */}
-        {/* <MemberDAOSection /> */}
+        <IntroSection />
+        <CommunitySection />
+        <SocialLinksSection />
+        <MemberDAOSection />
       </main>
     </Layout>
   );
