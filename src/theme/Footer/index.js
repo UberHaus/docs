0bb4720fb211d43400192e9717f0922ff5a1c8f6
jonathanprozzi <jonathanprozzi@gmail.com>
2021-07-21 23:54:02 -0400

    cleaned up Footer

--- src/theme/Footer/index.js
+++ src/theme/Footer/index.js
@@ -4,13 +4,15 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
+
+// this component is swizzled -- code added by the UberHaus team based on the original Facebook code
+
 import React from "react";
-import clsx from "clsx";
+
 import Link from "@docusaurus/Link";
 import { useThemeConfig } from "@docusaurus/theme-common";
 import useBaseUrl from "@docusaurus/useBaseUrl";
 import styles from "./styles.module.css";
-import ThemedImage from "@theme/ThemedImage";
 import {
   FooterTwitterIcon,
   FooterDiscordIcon,
@@ -43,12 +45,7 @@ function FooterLink({ to, href, label, prependBaseUrlToHref, ...props }) {
 
 function Footer() {
   const { footer } = useThemeConfig();
-  const { copyright, links = [], logo = {} } = footer || {};
-  console.log("links:", footer.links);
-  const sources = {
-    light: useBaseUrl(logo.src),
-    dark: useBaseUrl(logo.srcDark || logo.src),
-  };
+  const { links = [], logo = {} } = footer || {};
 
   if (!footer) {
     return null;
@@ -167,8 +164,6 @@ function Footer() {
             ))}
           </>
         )}
-        {/* <div>hi</div> */}
-        {/* <div>hi</div> */}
       </div>
       <div className={styles.copyrightContainer}>
         <span className={styles.copyrightText}>
@@ -184,61 +179,3 @@ function Footer() {
 }
 
 export default Footer;
-
-// {links && links.length > 0 && (
-//   <div className='row footer__links'>
-//     {links.map((linkItem, i) => (
-//       <div key={i} className='col footer__col'>
-//         {linkItem.title != null ? (
-//           <h4 className='footer__title'>{linkItem.title}</h4>
-//         ) : null}
-//         {linkItem.items != null &&
-//         Array.isArray(linkItem.items) &&
-//         linkItem.items.length > 0 ? (
-//           <ul className='footer__items'>
-//             {linkItem.items.map((item, key) =>
-//               item.html ? (
-//                 <li
-//                   key={key}
-//                   className='footer__item' // Developer provided the HTML, so assume it's safe.
-//                   // eslint-disable-next-line react/no-danger
-//                   dangerouslySetInnerHTML={{
-//                     __html: item.html,
-//                   }}
-//                 />
-//               ) : (
-//                 <li key={item.href || item.to} className='footer__item'>
-//                   <FooterLink {...item} />
-//                 </li>
-//               )
-//             )}
-//           </ul>
-//         ) : null}
-//       </div>
-//     ))}
-//   </div>
-// )}
-// {(logo || copyright) && (
-//   <div className='footer__bottom text--center'>
-//     {logo && (logo.src || logo.srcDark) && (
-//       <div className='margin-bottom--sm'>
-//         {logo.href ? (
-//           <Link href={logo.href} className={styles.footerLogoLink}>
-//             <FooterLogo alt={logo.alt} sources={sources} />
-//           </Link>
-//         ) : (
-//           <FooterLogo alt={logo.alt} sources={sources} />
-//         )}
-//       </div>
-//     )}
-//     {copyright ? (
-//       <div
-//         className='footer__copyright' // Developer provided the HTML, so assume it's safe.
-//         // eslint-disable-next-line react/no-danger
-//         dangerouslySetInnerHTML={{
-//           __html: copyright,
-//         }}
-//       />
-//     ) : null}
-//   </div>
-// )}
